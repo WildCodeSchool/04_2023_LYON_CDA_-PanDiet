b073bab761c8b23b51dd33c7b3f137a151415e0c
Sahrane_Guassemi <sahranedev@gmail.com>
2023-04-12 10:56:37 +0200

    add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import NavBar from "./components/NavBar";
+import ChooseDiet from "./pages/ChooseDiet";
+import Fridge from "./pages/Fridge";
+import Favourites from "./pages/Favourites";
+import Team from "./pages/Team";
+import HomeDescription from "./pages/HomeDescription";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <p>coucou</p>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home-description" element={<HomeDescription />} />
+        <Route path="/choose-your-diet" element={<ChooseDiet />} />
+        <Route path="/my-fridge" element={<Fridge />} />
+        <Route path="/favourites" element={<Favourites />} />
+        <Route path="/the-team" element={<Team />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
