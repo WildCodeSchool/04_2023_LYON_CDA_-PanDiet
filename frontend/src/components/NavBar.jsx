7d9edcbe90bc5fa326d677a23ceb53738fbf719c
ryanrasal <ryan.beaujot@outlook.fr>
2023-04-12 12:09:36 +0200

    finished home page and HomeDescription

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,7 +1,124 @@
-import React from "react";
+import * as React from "react";
+import { Link } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import "../App.css";
 
-function NavBar() {
-  return <div />;
+const drawerWidth = 240;
+const navItems = [
+  { label: "Home", to: "/" },
+  { label: "My Diet", to: "/choose-your-diet" },
+  { label: "Favourites", to: "/favourites" },
+  { label: "My fridge", to: "/my-fridge" },
+  { label: "The Team", to: "/the-team" },
+];
+
+function NavBar(props) {
+  const window = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        <span style={{ color: "#DC965A", fontWeight: "bold" }}>P</span>an
+        <span style={{ color: "#DC965A", fontWeight: "bold" }}>D</span>iet
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item.label} disablePadding>
+            <ListItemButton
+              component={Link}
+              to={item.to}
+              sx={{ textAlign: "center" }}
+            >
+              <ListItemText primary={item.label} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        component="nav"
+        sx={{ backgroundColor: "#242325", position: "inherit" }}
+      >
+        <Toolbar sx={{ display: "flex", justifyContent: "end" }}>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ display: { xs: "none", sm: "block" } }}
+          >
+            <span style={{ color: "#DC965A", fontWeight: "bold" }}>P</span>an{" "}
+            <span style={{ color: "#DC965A", fontWeight: "bold" }}>D</span>iet
+          </Typography>
+          <Box sx={{ display: { xs: "none", sm: "fixed" } }}>
+            {navItems.map((item) => (
+              <Button
+                key={item.label}
+                component={Link}
+                to={item.to}
+                sx={{ color: "#fff" }}
+              >
+                {item.label}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </Box>
+  );
 }
 
 export default NavBar;
