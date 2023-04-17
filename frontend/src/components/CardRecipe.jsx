1e03b6f214b5bbd05527200a9dab8bf2971c8672
ryanrasal <ryan.beaujot@outlook.fr>
2023-04-17 09:51:27 +0200

    finished page results and components recipeCard

--- frontend/src/components/CardRecipe.jsx
+++ frontend/src/components/CardRecipe.jsx
@@ -1,12 +1,10 @@
-/* eslint-disable react/self-closing-comp */
 import React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import gateauChocolat from "../assets/gateauChocolat.jpeg";
 
-function CardRecipe() {
+function CardRecipe({ item }) {
   return (
     <div>
       <div
@@ -16,17 +14,16 @@ function CardRecipe() {
           margin: "5% auto",
           width: "70%",
         }}
-      ></div>
-
+      />
       <Card sx={{ maxWidth: 345, margin: "0 auto" }}>
         <CardMedia
           sx={{ height: 140 }}
-          image={gateauChocolat}
+          image={item.recipe.image}
           title="green iguana"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            TITLE
+            {item.recipe.label}
           </Typography>
         </CardContent>
       </Card>
