6ec3d452ff9173be6924452831615621368b13e7
Brice <brice.verriere@epitech.eu>
2023-04-17 10:53:34 +0200

    my fridge front et team front

--- frontend/src/pages/Fridge.jsx
+++ frontend/src/pages/Fridge.jsx
@@ -1,7 +1,105 @@
 import React from "react";
+import Card from "@mui/material/Card";
+import CardActionArea from "@mui/material/CardActionArea";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import "../App.css";
+
+import desserts from "../assets/desserts.jpg";
+import meals from "../assets/repas.jpeg";
+import aperitifs from "../assets/apero.jpg";
+
+const style = {
+  global: {
+    backgroundColor: "#242325",
+    height: "100vh",
+  },
+  title: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "white",
+    fontWeight: "bold",
+    backgroundColor: "#242325",
+    fontSize: 35,
+  },
+  cardText: {
+    position: "absolute",
+    color: "white",
+    top: 45,
+    left: "50%",
+    transform: "translateX(-50%)",
+    fontWeight: "bold",
+    fontSize: 35,
+  },
+  cardMedia: {
+    opacity: 0.3,
+    borderRadius: 2.5,
+  },
+  cards: {
+    minWidth: 275,
+    backgroundColor: "#242325",
+    margin: "5% auto",
+    width: "70%",
+  },
+};
 
 function Fridge() {
-  return <div />;
+  return (
+    <div style={style.global}>
+      <Typography variant="h3" sx={style.title}>
+        My Fridge
+      </Typography>
+
+      <Card sx={style.cards}>
+        <CardActionArea>
+          <CardContent>
+            <div style={{ position: "relative" }}>
+              <CardMedia
+                sx={style.cardMedia}
+                component="img"
+                height="140"
+                image={aperitifs}
+                alt="aperitifs"
+              />
+              <div style={style.cardText}>Aperitifs</div>
+            </div>
+          </CardContent>
+        </CardActionArea>
+
+        <CardActionArea>
+          <CardContent>
+            <div style={{ position: "relative" }}>
+              <CardMedia
+                sx={style.cardMedia}
+                component="img"
+                height="140"
+                image={meals}
+                alt="meals"
+              />
+              <div style={style.cardText}>Meals</div>
+            </div>
+          </CardContent>
+        </CardActionArea>
+
+        <CardActionArea>
+          <CardContent>
+            <div style={{ position: "relative" }}>
+              <CardMedia
+                sx={style.cardMedia}
+                component="img"
+                height="140"
+                image={desserts}
+                alt="desserts"
+              />
+              <div style={style.cardText}>Desserts</div>
+            </div>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </div>
+  );
 }
 
 export default Fridge;
--- frontend/src/pages/Team.jsx
+++ frontend/src/pages/Team.jsx
@@ -1,7 +1,72 @@
 import React from "react";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import ImageListItemBar from "@mui/material/ImageListItemBar";
+import Typography from "@mui/material/Typography";
 
 function Team() {
-  return <div />;
+  const style = {
+    container: {
+      backgroundColor: "#242325",
+      height: "100vh",
+    },
+    imageLists: {
+      backgroundColor: "#242325",
+      width: 500,
+      height: 450,
+      color: "white",
+      fontWeight: "bold",
+    },
+    title: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      color: "white",
+      fontWeight: "bold",
+      backgroundColor: "#242325",
+      fontSize: 35,
+    },
+  };
+
+  const itemData = [
+    {
+      img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+      title: "Ryan",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+      title: "Sahrane",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+      title: "Hamza",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+      title: "Brice",
+    },
+  ];
+
+  return (
+    <div style={style.container}>
+      <Typography variant="h3" sx={style.title}>
+        The Team !
+      </Typography>
+      <ImageList sx={style.imageLists}>
+        {itemData.map((item) => (
+          <ImageListItem key={item.img}>
+            <img
+              src={`${item.img}?w=248&fit=crop&auto=format`}
+              srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              alt={item.title}
+              loading="lazy"
+            />
+            <ImageListItemBar title={item.title} position="below" />
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </div>
+  );
 }
 
 export default Team;
