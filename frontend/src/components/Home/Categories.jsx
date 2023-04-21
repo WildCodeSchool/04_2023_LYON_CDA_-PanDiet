c6ca6203698e0ddc238bd2c9a9f33b9732c9d540
ryanrasal <ryan.beaujot@outlook.fr>
2023-04-21 16:02:20 +0200

    refont categories design

--- frontend/src/components/Home/Categories.jsx
+++ frontend/src/components/Home/Categories.jsx
@@ -1,24 +1,132 @@
 import React from "react";
-import breakFast from "../../assets/home/breakFast.png";
-import snack from "../../assets/home/snack.png";
-import dinner from "../../assets/home/dinner.svg";
-import tea from "../../assets/home/tea.svg";
-import CardCategory from "./CardCategory";
+import { styled } from "@mui/material/styles";
+import ButtonBase from "@mui/material/ButtonBase";
+import Typography from "@mui/material/Typography";
+// import breakFast from "../../assets/breakfast.jpeg";
+// import snack from "../../assets/snack.jpg";
+// import dinner from "../../assets/dinner.jpeg";
+// import tea from "../../assets/teaTime.jpg";
+
+const images = [
+  {
+    url: "https://urlz.fr/lxqy",
+    title: "breakfast",
+    width: "30%",
+  },
+  {
+    url: "https://urlz.fr/lxqk",
+    title: "snack",
+    width: "30%",
+  },
+  {
+    url: "https://urlz.fr/lxqt",
+    title: "dinner",
+    width: "30%",
+  },
+  {
+    url: "https://urlz.fr/lxqp",
+    title: "teaTime",
+    width: "30%",
+  },
+];
+
+const ImageButton = styled(ButtonBase)(({ theme }) => ({
+  position: "relative",
+  height: 200,
+  [theme.breakpoints.down("sm")]: {
+    width: "30vw !important", // Overrides inline-style
+    height: 100,
+  },
+  "&:hover, &.Mui-focusVisible": {
+    zIndex: 1,
+    "& .MuiImageBackdrop-root": {
+      opacity: 0.15,
+    },
+    "& .MuiImageMarked-root": {
+      opacity: 0,
+    },
+    "& .MuiTypography-root": {
+      border: "4px solid currentColor",
+    },
+  },
+}));
+
+const ImageSrc = styled("span")({
+  position: "absolute",
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundSize: "cover",
+  backgroundPosition: "center 40%",
+});
+
+const Image = styled("span")(({ theme }) => ({
+  position: "absolute",
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  color: theme.palette.common.white,
+}));
+
+const ImageBackdrop = styled("span")(({ theme }) => ({
+  position: "absolute",
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundColor: theme.palette.common.black,
+  opacity: 0.4,
+  transition: theme.transitions.create("opacity"),
+}));
+
+const ImageMarked = styled("span")(({ theme }) => ({
+  height: 3,
+  width: 18,
+  backgroundColor: theme.palette.common.white,
+  position: "absolute",
+  bottom: -2,
+  left: "calc(50% - 9px)",
+  transition: theme.transitions.create("opacity"),
+}));
 
 function Categories({ handleClickCategory }) {
-  const categories = [
-    { id: 1, name: "breakfast", img: breakFast },
-    { id: 2, name: "snack", img: snack },
-    { id: 3, name: "dinner", img: dinner },
-    { id: 4, name: "teaTime", img: tea },
-  ];
-
   return (
     <div>
       <h2 className="font-bold pl-3 pb-5 text-xl shadow-sm">Catégories</h2>
       <div className="flex md:justify-around overflow-auto">
-        {categories.map((item) => (
-          <CardCategory item={item} handleClickCategory={handleClickCategory} />
+        {images.map((image) => (
+          <ImageButton
+            onClick={() => handleClickCategory(image.title)}
+            focusRipple
+            key={image.title}
+            style={{
+              width: image.width,
+            }}
+          >
+            <ImageSrc style={{ backgroundImage: `url(${image.url})` }} />
+            <ImageBackdrop className="MuiImageBackdrop-root" />
+            <Image>
+              <Typography
+                component="span"
+                variant="subtitle1"
+                color="inherit"
+                sx={{
+                  position: "relative",
+                  p: 4,
+                  pt: 2,
+                  pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
+                }}
+              >
+                {image.title}
+                <ImageMarked className="MuiImageMarked-root" />
+              </Typography>
+            </Image>
+          </ImageButton>
         ))}
       </div>
     </div>
