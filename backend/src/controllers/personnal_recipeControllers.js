f86cfc723b016bec5f83b2ac293e25b4535969c1
Sahrane_Guassemi <sahranedev@gmail.com>
2023-04-26 10:08:07 +0200

    creation database + back feature for user (registration, auth,  token, search, delete, etc..) + myrecipe feature (CRUD)

---
+++ backend/src/controllers/personnal_recipeControllers.js
@@ -0,0 +1,82 @@
+const models = require("../models");
+
+const browse = (req, res) => {
+  models.personnal_recipe
+    .findAll()
+    .then(([results]) => {
+      res.send(results);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.sendStatus(500);
+    });
+};
+
+const read = (req, res) => {
+  const { id } = req.params;
+
+  models.personnal_recipe
+    .find(id)
+    .then(([results]) => {
+      if (results[0]) res.send(results[0]);
+      else res.sendStatus(404);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.sendStatus(500);
+    });
+};
+
+const add = (req, res) => {
+  const recipe = req.body;
+
+  // on verifie les données
+
+  models.personnal_recipe
+    .insert(recipe)
+    .then(([result]) => {
+      res.location(`/api/my-recipes/${result.insertId}`).sendStatus(201);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.sendStatus(500);
+    });
+};
+
+const edit = (req, res) => {
+  const recipe = req.body;
+  recipe.id = req.params.id;
+
+  models.personnal_recipe
+    .update(recipe)
+    .then(([result]) => {
+      if (result.affectedRows === 0) res.sendStatus(404);
+      else res.sendStatus(204);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.sendStatus(500);
+    });
+};
+
+const destroy = (req, res) => {
+  const { id } = req.params;
+  models.personnal_recipe
+    .delete(id)
+    .then(([result]) => {
+      if (result.affectedRows === 0) res.sendStatus(404);
+      else res.sendStatus(204);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.sendStatus(500);
+    });
+};
+
+module.exports = {
+  browse,
+  read,
+  add,
+  edit,
+  destroy,
+};
--- backend/src/router.js
+++ backend/src/router.js
@@ -3,11 +3,39 @@ const express = require("express");
 const router = express.Router();
 
 const itemControllers = require("./controllers/itemControllers");
+const userControllers = require("./controllers/userControllers");
+const personnalRecipeControllers = require("./controllers/personnal_recipeControllers");
+const authControllers = require("./controllers/authControllers");
+const { hashPassword, verifyPassword } = require("./services/auth");
+
+/// / login ////
+
+router.post(
+  "/api/login",
+  authControllers.getUserByEmailWithPasswordAndPassToNext,
+  verifyPassword
+);
 
 router.get("/items", itemControllers.browse);
 router.get("/items/:id", itemControllers.read);
 router.put("/items/:id", itemControllers.edit);
 router.post("/items", itemControllers.add);
 router.delete("/items/:id", itemControllers.destroy);
+
+/// // GESTION DES USERS ////
+
+router.get("/api/users", userControllers.browse);
+router.get("/api/users/:id", userControllers.read);
+router.put("/api/users/:id", userControllers.edit);
+router.post("/api/users", hashPassword, userControllers.add);
+router.delete("/api/users/:id", userControllers.destroy);
+
+/// // GESTION DES RECETTES PERSONNELLES /////
+
+router.get("/api/my-recipes", personnalRecipeControllers.browse);
+router.get("/api/my-recipes/:id", personnalRecipeControllers.read);
+router.put("/api/my-recipes/:id", personnalRecipeControllers.edit);
+router.post("/api/my-recipes", personnalRecipeControllers.add);
+router.delete("/api/my-recipes/:id", personnalRecipeControllers.destroy);
 
 module.exports = router;
